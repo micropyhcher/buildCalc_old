537f963ade0774db631c86aad7f5ae8ffad2c01d
micropyhcher <32598857+micropyhcher@users.noreply.github.com>
2019-03-19 10:44:38 +0300

    Delete isert.sql

--- target/classes/db/isert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO buildcalc.users (id, name, age, email, pass) VALUES (1, 'Maxim', 37, 'max@work.by', '123123');
-INSERT INTO buildcalc.users (id, name, age, email, pass) VALUES (2, 'Dima', 33, 'dima@work.ua', '123123');
-INSERT INTO buildcalc.users (id, name, age, email, pass) VALUES (3, 'Lena', 35, 'lena@home.by', '123123');
-INSERT INTO buildcalc.users (id, name, age, email, pass) VALUES (4, 'Ira', 14, 'ira@achool.by', '123123');
